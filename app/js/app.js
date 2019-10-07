65917486cb539f1693ea4e3726f4cb4e5ca69714
SerezhaWEB <hovorovskyi@gmail.com>
2019-10-07 13:04:29 +0300

    visually done

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,36 @@
 window.addEventListener('load', function() {
 
-   let container = document.querySelector('.customers-container');
-   let tablist = container.querySelector('[role=tablist]');
-   let tabs = container.querySelectorAll('[role=tab]');
-   let tabpanels = container.querySelectorAll('.customer-testis__testi');
+   const container = document.querySelector('.customers-container');
+   const tablist = container.querySelector('[role=tablist]');
+   const tabs = container.querySelectorAll('[role=tab]');
+   const tabpanels = container.querySelectorAll('.customer-testis__testi');
    let activeTab = container.querySelector('[role=tab][aria-selected=true]');
+   const header = document.querySelector('.header');
+   const burgerBtn = header.querySelector('.burger-button');
+   const mobileNav = header.querySelector('.mobile-nav');
+   const mobileItems = mobileNav.querySelectorAll('.mobile-nav__item');
+   const anchorLinks = document.querySelectorAll('[data-scroll="anchor"]')
+
+
+   burgerBtn.addEventListener('click', function() {
+      this.classList.toggle('active');
+      header.classList.toggle('active');
+      mobileNav.classList.toggle('active');
+   })
+
+   anchorLinks.forEach(anchorLink => {
+   anchorLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = this.getAttribute('href');
+      const scrollToEl = document.querySelector(id);
+      scrollToEl.scrollIntoView();
+      if (window.innerWidth <= 650) {
+         mobileNav.classList.remove('active');
+         burgerBtn.classList.remove('active');
+         header.classList.remove('active');
+      }
+   })
+});
 
    tabs.forEach(function(tab) {
       tab.addEventListener('click', function(e) {
@@ -32,13 +58,13 @@ window.addEventListener('load', function() {
             let next = [...tabs].indexOf(activeTab) + 1;
             next = next < tabs.length ? next : 0
             setActiveTab(tabs[next].getAttribute('aria-controls'));
-            break;         
-            case 37:
-               e.preventDefault();
-               let previous = [...tabs].indexOf(activeTab) - 1;
-               previous = previous >= 0 ? previous : tabs.length - 1
-               setActiveTab(tabs[previous].getAttribute('aria-controls'));
-               break;
+            break;
+         case 37:
+            e.preventDefault();
+            let previous = [...tabs].indexOf(activeTab) - 1;
+            previous = previous >= 0 ? previous : tabs.length - 1
+            setActiveTab(tabs[previous].getAttribute('aria-controls'));
+            break;
          default:
             return;
       }
